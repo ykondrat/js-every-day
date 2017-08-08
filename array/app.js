bcec3d8bdfd3b27f86ee747865ebdc0bf5333e32
Yevhen Kondratyev <kondratyev.yevhen@gmail.com>
2017-08-08 10:14:51 +0300

    js flex

--- array/app.js
+++ array/app.js
@@ -32,3 +32,39 @@ const totalYears = inventors.reduce((total, inventor) => {
 }, 0);
 
 console.log(totalYears);
+
+const oldest = inventors.sort((a, b) => {
+    const last = a.passed - a.year;
+    const next = b.passed - a .year;
+    
+    return (last > next ? -1 : 1);
+});
+
+console.table(oldest);
+
+//const category = document.querySelector('.mw-category');
+//const links = Array.from(category.querySelectorAll('a'));
+//
+//const de = links.map(link => link.textContent).filter(streetName => streetName.includes('de'));
+
+
+const alpha = people.sort(function(lastOne, nextOne) {
+    const [alast, afirst] = lastOne.split(', ');
+    const [blast, bfirst] = nextOne.split(', ');
+    
+    return (alast > blast ? 1 : -1);
+});
+
+console.log(alpha);
+
+const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck'];
+
+const transportation = data.reduce(function(obj, item) {
+    if (!obj[item]) {
+        obj[item] = 0;
+    }
+    obj[item]++;
+    return (obj);
+}, {});
+
+console.log(transportation);
