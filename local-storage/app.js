43118081ca0fe029ff501aa51d56c437fbded094
Yevhen Kondratyev <kondratyev.yevhen@gmail.com>
2017-09-28 18:00:51 +0300

    local

---
+++ local-storage/app.js
@@ -0,0 +1,38 @@
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const items = JSON.parse(localStorage.getItem('items')) || [];
+
+function addItem(e) {
+    e.preventDefault();
+    const text = (this.querySelector('[name=item]')).value;
+    const item = {
+        text: text,
+        done: false
+    };
+    items.push(item);
+    populateList(items, itemsList);
+    localStorage.setItem('items', JSON.stringify(items));
+    this.reset();
+}
+
+function populateList(plates = [], platesList) {
+    platesList.innerHTML = plates.map((plate, index) => {
+        return `
+            <li>
+                <input type="checkbox" data-index=${index} id="item${index}" ${plate.done ? 'checked' : ''} />
+                <label for="item${index}">${plate.text}</label>
+            </li>
+        `;
+    }).join('');
+}
+
+function toggleDone(e) {
+    if (!e.target.matches('input')) return;
+    items[e.target.dataset.index].done = !items[e.target.dataset.index].done;
+    localStorage.setItem('items', JSON.stringify(items));
+    populateList(items, itemsList);
+}
+
+addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
+populateList(items, itemsList);
